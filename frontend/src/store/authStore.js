dd9dd2b611a3a54ce1c89300440970f6487274ee
Muteeb Haider <muteebhdr@gmail.com>
2025-04-21 22:10:27 +0500

    Build Dashboard with logout functinality

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -55,6 +55,22 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+
+  logout: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({
+        user: null,
+        isAuthenticated: false,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({ error: "Error Logging out", isLoading: false });
+      throw error;
+    }
+  },
   verifyEmail: async (code) => {
     // Start loading and reset any previous errors
     set({ isLoading: true, error: null });
